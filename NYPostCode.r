21a2712fbdd9f4db847a4769f9b5f6c87f8bf9b9
letty <ltcancel@gmail.com>
2023-03-15 20:05:45 -0400

    update

--- NYPostCode.r
+++ NYPostCode.r
@@ -16,7 +16,7 @@ nyp %>%
   html_text()
 
 #page section
-page2 %>%
+nyp2 %>%
   html_nodes(".section-heading--underline") %>%
   html_text()
 
@@ -26,16 +26,17 @@ nyp %>%
   html_nodes(".story__headline.headline.headline--archive") %>%
   html_text()
 
-page2 %>%
+nyp2 %>%
   html_nodes(".story__headline.headline.headline--archive") %>%
   html_text()
 
 #article links
-nyp %>%
+links <- nyp %>%
   #html_nodes(".layout__item a") %>%
   html_nodes(".headline--archive a") %>%
   html_attr("href") %>%
   as.character()
+
 
 #article dates
 nyp %>%
@@ -49,18 +50,18 @@ art1 <- read_html("https://nypost.com/2022/11/10/trump-says-hes-still-a-stable-g
 # article 1 date
 art1 %>%
   html_node("div.date.meta.meta--byline") %>%
-  html_text()
+  html_text(trim = TRUE)
 
 #article 1 text
 art1 %>%
   #html_node(xpath='//*[@id="main"]/article/div/div/div[1]/div/div[3]') %>%
   html_node("div.single__content.entry-content.m-bottom") %>%
-  html_text()
+  html_text(trim = TRUE)
 
 #article 1 tag list
 art1 %>%
   html_nodes("li.tag-list__tag") %>%
-  html_text()
+  html_text(trim = TRUE)
 
 #article 2 test
 art2 <- read_html("https://nypost.com/2021/12/14/hypocrisy-of-media-censors-claim-to-be-against-misinformation/")
@@ -74,3 +75,73 @@ art2 %>%
 art2 %>%
   html_nodes("li.tag-list__tag") %>%
   html_text()
+
+-----------------------------------------------------------------------------------------------------------------
+
+#get all article titles
+title <- nyp %>%
+  html_nodes(".story__headline.headline.headline--archive") %>%
+  html_text(trim = TRUE)
+
+#get article dates
+date <- nyp %>%
+  #html_nodes(".layout__item a") %>%
+  html_nodes("span.meta.meta--byline") %>%
+  html_text(trim = TRUE)
+
+#get article URLs
+link <- nyp %>%
+  #html_nodes(".layout__item a") %>%
+  html_nodes(".headline--archive a") %>%
+  html_attr("href") %>%
+  as.character()
+
+nyp_vector <- data.frame(title, date, link)
+
+article_list <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(article_list) <- c('title', 'date', 'link')
+article_list <- rbind(article_list, nyp_vector)
+head(article_list)
+
+
+
+-----------------------------------------------------------------------------------------------------------------
+  
+# function to collect articles
+  
+# empty dataframe with column names
+article_list <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(article_list) <- c('title', 'date', 'link' )
+
+
+articles <- function() {
+  #get url
+  url <-("https://nypost.com/tag/fake-news/page/1")
+  nyp <- read_html(url)
+  
+  #get all article titles
+  a <- nyp %>%
+    html_nodes(".story__headline.headline.headline--archive") %>%
+    html_text(trim = TRUE)
+  
+  #get article dates
+  b <- nyp %>%
+    html_nodes("span.meta.meta--byline") %>%
+    html_text(trim = TRUE)
+  
+  #get article URLs
+  c <- nyp %>%
+    html_nodes(".headline--archive a") %>%
+    html_attr("href") %>%
+    as.character()
+  
+  temp_df <- data.frame(a, b, c)
+  article_list <- rbind(article_list, temp_df)
+  
+  return(article_list)
+}
+
+articles_return <- articles()
+
+head(articles_return)
+ 
